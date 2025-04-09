678337d7c3239d2e9e9e9fbe5cc86547b363cf58
ZainabAA <zainab.a.a.alarbash@gmail.com>
2025-04-09 11:34:25 +0300

    added graphics

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { powerups } from '../data/powerups';
+import { MatButtonModule } from '@angular/material/button';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './start.component.html',
-  styleUrl: './start.component.css'
+  styleUrl: './start.component.css',
 })
 export class StartComponent {
   @Output() setTotalScore = new EventEmitter<number>();
@@ -15,6 +17,24 @@ export class StartComponent {
   currentInceremt = 1;
   boosterName = "";
   notUsedPowerUps = powerups;
+  gifSource = ['/assets/gifFrames/frame_00_delay-0.03s.gif',
+    '/assets/gifFrames/frame_01_delay-0.03s.gif',  
+    '/assets/gifFrames/frame_02_delay-0.03s.gif', '/assets/gifFrames/frame_03_delay-0.03s.gif', 
+    '/assets/gifFrames/frame_04_delay-0.03s.gif', '/assets/gifFrames/frame_05_delay-0.03s.gif', 
+    '/assets/gifFrames/frame_06_delay-0.03s.gif', '/assets/gifFrames/frame_07_delay-0.03s.gif', 
+    '/assets/gifFrames/frame_08_delay-0.03s.gif', '/assets/gifFrames/frame_09_delay-0.03s.gif',
+    '/assets/gifFrames/frame_10_delay-0.03s.gif', '/assets/gifFrames/frame_11_delay-0.03s.gif',
+    '/assets/gifFrames/frame_12_delay-0.03s.gif', '/assets/gifFrames/frame_13_delay-0.03s.gif',
+    '/assets/gifFrames/frame_15_delay-0.03s.gif', '/assets/gifFrames/frame_16_delay-0.03s.gif',
+    '/assets/gifFrames/frame_17_delay-0.03s.gif', '/assets/gifFrames/frame_18_delay-0.03s.gif', 
+    '/assets/gifFrames/frame_19_delay-0.03s.gif', '/assets/gifFrames/frame_20_delay-0.03s.gif',
+    '/assets/gifFrames/frame_21_delay-0.03s.gif', '/assets/gifFrames/frame_22_delay-0.03s.gif',
+    '/assets/gifFrames/frame_23_delay-0.03s.gif', '/assets/gifFrames/frame_24_delay-0.03s.gif',
+    '/assets/gifFrames/frame_25_delay-0.03s.gif', '/assets/gifFrames/frame_26_delay-0.03s.gif',
+    '/assets/gifFrames/frame_27_delay-0.03s.gif', '/assets/gifFrames/frame_28_delay-0.03s.gif',
+    '/assets/gifFrames/frame_29_delay-0.03s.gif']
+  currentFrameIndex = 0;
+
 
   updateScore(incremntAmount: number){
     this.currentScore += incremntAmount;
@@ -22,6 +42,7 @@ export class StartComponent {
     if(pUp){
       this.boosterName = pUp.booster;
     }
+    this.currentFrameIndex = (this.currentFrameIndex + 1)%this.gifSource.length;
   }
 
   setPowerup(name: string){
